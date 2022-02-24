14f59ece778dd0d442b266cd1018c059d600e4af
isac260666 <isacsilvacosta977@gmail.com>
2022-02-24 16:02:20 -0300

    Atividade 01

--- src/app/PastaTeste.java
+++ src/app/PastaTeste.java
@@ -11,17 +11,17 @@ public class Main {
         Funcionario func1 = new Funcionario();
         Funcionario func2 = new Funcionario();
         
-        func1.setNome("Daniel");
-        func1.setIdade(29);
-        func1.setSalario(300000f);
-        func1.setMatricula(123456);
+        func1.setNome("Francisco");
+        func1.setIdade(60);
+        func1.setSalario(400000f);
+        func1.setMatricula(145654);
         func1.setSexo('M');
         
-        func2.setNome("Daniela");
+        func2.setNome("Calos");
         func2.setIdade(40);
         func2.setSalario(500000.00f);
-        func2.setMatricula(654321);
-        func2.setSexo('F');
+        func2.setMatricula(843237);
+        func2.setSexo('M');
         
         System.out.println(func1.toString());
         System.out.println("*************************************************");
